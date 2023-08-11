2f3b91877e6efc688dd4bc3598e7b95b448ac773
Kiongo-Bob <njoguub@gmail.com>
2023-08-11 11:50:03 +0300

    Cypress

---
+++ Week 6/frontend/app.spec.js
@@ -0,0 +1,22 @@
+import 'cypress'
+
+describe("It should not be clickable", ()=>{
+    it('should be disabled when inputs are empty', () => {
+        cy.visit('http://127.0.0.1:5500/Week%206/frontend/input.html?');
+
+        cy.get('#submit').should('be.disabled');
+    
+    
+        cy.get('#fname').type('Kiongo');
+        cy.get('#sname').type('Bob');
+        cy.get('#cohortNo').type('10');
+    
+        cy.get('#submit').should('not.be.disabled');
+      });
+    });
+  
+    
+    
+    
+    
+    
