674800c53f242507f451f1e9b22a81942cfe1466
Kiongo-Bob <njoguub@gmail.com>
2023-08-11 11:37:33 +0300

    Week 6 Assessment

--- Week 6/middleware/verifyUserInput.js
+++ Week 6/middleware/verifyUserInput.js
@@ -1,7 +1,7 @@
 const validateUserRegistration = (req, res, next) => {
-    const { Username, Email, Password } = req.body
+    const {Email} = req.body
 
-    if (!Username || !Email || !Password) {
+    if (!Email ||) {
         return res.status(400).json({
             message: "Ensure all fields are filled"
         })
@@ -17,25 +17,6 @@ const validateUserRegistration = (req, res, next) => {
     next();
 }
 
-
-const validateUserLogin = (req, res, next) => {
-    const { Email, Password } = req.body
-
-    if (!Email || !Password) {
-        return res.status(400).json({
-            message: "All fields are required"
-        })
-    }
-
-    if (!validateEmailFormat(Email)) {
-        return res.status(400).json({
-            message: "Invalid email format"
-        })
-    }
-
-    next();
-}
-
 const validateEmailFormat = (email) => {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
@@ -43,5 +24,4 @@ const validateEmailFormat = (email) => {
 
 module.exports = {
     validateUserRegistration,
-    validateUserLogin
 }
